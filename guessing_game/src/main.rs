e79f2a891a359ccac410b9a188d738a2b1c802a0
mzaneri <michaelzaneri@gmail.com>
2019-04-24 13:51:08 -0400

    added matching statement to check guss vs secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,11 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret = rand::thread_rng().gen_range(1, 101);
 
     println!("Please input your guess.");
 
@@ -9,5 +13,13 @@ fn main() {
 
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("Too small!, secret was {}", secret),
+        Ordering::Greater => println!("Too big!, secret was {}", secret),
+        Ordering::Equal => println!("You win!"),
+    }
 }
